8a24b66bb142df9b6d6617d1b3f5eb96e57b656e
Riyaj K M <80973593+mrriyaj@users.noreply.github.com>
2025-10-09 22:46:54 +0530

    Refactor Tailwind CSS configuration and remove unused utilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
-import preset from './vendor/filament/support/tailwind.config.preset';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    presets: [preset],
     content: [
         './app/Filament/**/*.php',
+        './app/Forms/Components/**/*.php',
+        './app/Livewire/**/*.php',
+        './app/Infolists/Components/**/*.php',
+        './app/Providers/Filament/**/*.php',
+        './app/Tables/Columns/**/*.php',
         './resources/views/**/*.blade.php',
         './resources/views/filament/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
@@ -21,8 +24,20 @@ export default {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
             colors: {
+                primary: {
+                    50: 'rgb(var(--filament-color-primary-50) / <alpha-value>)',
+                    100: 'rgb(var(--filament-color-primary-100) / <alpha-value>)',
+                    200: 'rgb(var(--filament-color-primary-200) / <alpha-value>)',
+                    300: 'rgb(var(--filament-color-primary-300) / <alpha-value>)',
+                    400: 'rgb(var(--filament-color-primary-400) / <alpha-value>)',
+                    500: 'rgb(var(--filament-color-primary-500) / <alpha-value>)',
+                    600: 'rgb(var(--filament-color-primary-600) / <alpha-value>)',
+                    700: 'rgb(var(--filament-color-primary-700) / <alpha-value>)',
+                    800: 'rgb(var(--filament-color-primary-800) / <alpha-value>)',
+                    900: 'rgb(var(--filament-color-primary-900) / <alpha-value>)',
+                    950: 'rgb(var(--filament-color-primary-950) / <alpha-value>)',
+                },
                 danger: defaultTheme.colors.red,
-                primary: defaultTheme.colors.blue,
                 success: defaultTheme.colors.green,
                 warning: defaultTheme.colors.yellow,
             },
